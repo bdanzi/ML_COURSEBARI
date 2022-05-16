bc5b1616cd9e653e853aadb0139c9914a31a7f96
FrancescoSivo <francescomarcopolo@hotmail.it>
2022-05-16 10:27:31 +0200

    ggiornamento codice per Kurtis

--- DNN1.py
+++ DNN1.py
@@ -5,7 +5,7 @@ from numpy import loadtxt
 from matplotlib import pyplot as plt
 from tensorflow.keras.models import  Sequential
 #from keras.models import Sequential  <<== errors
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 #from keras.layers import Dense   <<== gave errors
 from sklearn.metrics import roc_curve, auc, plot_roc_curve, mean_squared_error  ## To show results
 from sklearn.ensemble import AdaBoostClassifier
@@ -44,10 +44,27 @@ dat = loadtxt('NormDat_train.txt', delimiter=' ')
 y = dat[:,0]
 x = dat[:, 1:24]
 
+## Load test data into arrays.
+test = loadtxt('NormDat_test.txt', delimiter=' ')
+ytst = test[:,0]
+xtst = test[:, 1:24]
+
+## Set up classifier structure
+cl1 = Sequential()  ## type of classifier
+cl1.add(Dense( 100,  activation='relu'))    ## First hidden layer # of nodes; densely connected
+cl1.add(Dropout(rate=0.1))
+cl1.add(Dense(  37,  activation='relu'))    ## 2nd layer
+cl1.add(Dropout(rate=0.1))
+cl1.add(Dense(   7,  activation='relu'))
+cl1.add(Dense(   1,  activation='sigmoid')) ## Output layer uses sigmoid for classification
+
+
 ## Set up classifier structure
 cl = Sequential()  ## type of classifier
 cl.add(Dense( 100,  activation='relu'))    ## First hidden layer # of nodes; densely connected
+cl.add(Dropout(rate=0.1))
 cl.add(Dense(  37,  activation='relu'))    ## 2nd layer
+cl.add(Dropout(rate=0.1))
 cl.add(Dense(   7,  activation='relu'))
 cl.add(Dense(   1,  activation='sigmoid')) ## Output layer uses sigmoid for classification
 
@@ -55,37 +72,14 @@ cl.add(Dense(   1,  activation='sigmoid')) ## Output layer uses sigmoid for clas
 #cl.compile( loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'] )  ## <<==good
 
 cl.compile( loss='MeanSquaredError', optimizer='adam', metrics=['accuracy'] )   ## <<== good
+cl1.compile( loss='MeanSquaredError', optimizer='adam', metrics=['accuracy'] )   ## <<== good
 bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), algorithm="SAMME", n_estimators=700)
 
 #cl.compile( loss='Hinge', optimizer='adam', metrics=['accuracy'] )  ##<<== not so good
 
-OOB_train = np.zeros((len(x), 2))  ## Set up to collect Out-Of-Bag results
-OOBResults_train = cl.predict(x)
-for i in range(len(y)):
-    OOB_train[i,0] = y[i]
-    OOB_train[i,1] = OOBResults_train[i]
 
-np.savetxt('OOB_train.txt', OOB_train , fmt="%d  %.7f")  ##<<== test results are saved here!
-## Load test data into arrays.
-test = loadtxt('NormDat_test.txt', delimiter=' ')
-ytst = test[:,0]
-xtst = test[:, 1:24]
-OOB_test = np.zeros((len(ytst), 2))  ## Set up to collect Out-Of-Bag results
-
-OOBResults_test = cl.predict(xtst)  ## Get predictions from NN on test data.
-
-for i in range(len(ytst)):      ## Put results into array for plotting, writing,...
-    OOB_test[i,0] = ytst[i]
-    OOB_test[i,1] = OOBResults_test[i]
-    
-print("Testing predictions available: \n", cl.evaluate(xtst,ytst) )
-np.savetxt('OOB_test.txt', OOB_test , fmt="%d  %.7f")  ##<<== test results are saved here!
-
-## Preview
-for i in range(20):
-    print(OOBResults_test[i], ytst[i])
 Chunk=50         # how many events per chunk
-NumChuncks=10  #     .... times this determines number of training events used.
+NumChuncks=50  #     .... times this determines number of training events used.
 Scores_train=[]
 mse_train=[]
 mse_test=[]
@@ -122,7 +116,7 @@ print("Training Done")
 # the plot of the loss function on the validation set is also computed and plotted
 plt.rcParams['figure.figsize'] = (13,6)
 plt.plot(mlpfitted.history['loss'], label='loss train',color='green')
-#plt.plot(mlpfitted.history['val_loss'], label='loss validation',color='magenta')
+plt.plot(mlpfitted.history['val_loss'], label='loss validation',color='magenta')
 plt.title("Loss", fontsize=12,fontweight='bold', color='r')
 plt.legend(loc="upper right")
 plt.xlabel('Epoch')
@@ -152,10 +146,31 @@ plt.close()
 # =============================================================================
 
 if EPOCHS < 100:  LOQUACIOUS=1
-cl.fit(x,y,epochs=EPOCHS, batch_size=64, verbose=LOQUACIOUS, validation_split=0.3)
-cl.evaluate(x,y)
+cl1.fit(x,y,epochs=EPOCHS, batch_size=64, verbose=LOQUACIOUS, validation_split=0.3, callbacks = callback_list)
+cl1.evaluate(x,y)
 bdt.fit(x,y)
 print("Training Done")
+OOB_train = np.zeros((len(x), 2))  ## Set up to collect Out-Of-Bag results
+OOBResults_train = cl1.predict(x)
+for i in range(len(y)):
+    OOB_train[i,0] = y[i]
+    OOB_train[i,1] = OOBResults_train[i]
+
+np.savetxt('OOB_train.txt', OOB_train , fmt="%d  %.7f")  ##<<== test results are saved here!
+
+OOB_test = np.zeros((len(ytst), 2))  ## Set up to collect Out-Of-Bag results
+
+OOBResults_test = cl1.predict(xtst)  ## Get predictions from NN on test data.
+
+for i in range(len(ytst)):      ## Put results into array for plotting, writing,...
+    OOB_test[i,0] = ytst[i]
+    OOB_test[i,1] = OOBResults_test[i]
+## Preview
+for i in range(20):
+    print(OOBResults_test[i], ytst[i]) 
+print("Testing predictions available: \n", cl1.evaluate(xtst,ytst) )
+np.savetxt('OOB_test.txt', OOB_test , fmt="%d  %.7f")  ##<<== test results are saved here!
+
 
 #############################################################################################################################
 ## Plotting the Mean square error loss function vs size of training sample, increasing this number in steps of Chunk number##
